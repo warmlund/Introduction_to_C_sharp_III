8d222cf921ec7545281a7617fa3969162feada70
warmlund <emeliewarmlund@gmail.com>
2024-09-14 17:29:55 +0200

    Setup MediaPlayerDA structure

---
+++ MediaPlayerDA/Media.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayerDA
+{
+    /// <summary>
+    /// This class represents a media record
+    /// </summary>
+    public class Media
+    {
+        public int Id; 
+        public string Name; 
+        public string Description;
+        public string Format;
+    }
+}
