daf8d9e7162ef1906b98753338bb213180c7de19
warmlund <emeliewarmlund@gmail.com>
2024-09-14 18:48:43 +0200

    Set up structure for Business Layer BL

---
+++ MediaPlayerBL/Playlist.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MediaPlayerBL
+{
+    public class Playlist
+    {
+        public List<Media> MediaFiles { get; set; }
+        public string PlaylistName { get; set; }
+    }
+}
