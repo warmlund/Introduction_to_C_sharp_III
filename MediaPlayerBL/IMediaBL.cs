d2f4ed6e77ad072d4bd8ca99a0ce4ee6c116ff2c
warmlund <emeliewarmlund@gmail.com>
2024-09-28 22:41:08 +0200

    Cleaned up unused logic in BL layer

--- MediaPlayerBL/IMediaBL.cs
+++ MediaPlayerBL/IMediaBL.cs
@@ -12,23 +12,11 @@ namespace MediaPlayerBL
     {
         List<Media> LoadMedia(string[] filenames);
         List<Media> LoadPlaylist(string filepath);
-        Task PlayMediaAsync(List<Media> loadedMedia);
         BitmapImage CreateImage(string filepath);
         Uri CreateVideo(string filepath);   
         void SavePlaylist(string filepath, List<Media> loadedMedia);
-        void PauseMedia();
         string GetPlaylistTitle();
-        void SetPlaylistTitle(string title);
-        void ResetIndex();
         bool IsImageFormat(string filepath);
         bool IsVideoFormat(string filepath);
-
-        event Action<Media> MediaChanged;
-        event Action<string> FormatChanged;
-
-        int PlaySpeed { get; set; }
-        bool IsPlaying { get; set; }
-        Media CurrentPlayingMedia { get; set; }
-        string CurrentFormat { get; set; }
     }
 }
--- MediaPlayerBL/MediaBL.cs
+++ MediaPlayerBL/MediaBL.cs
@@ -13,23 +13,11 @@ namespace MediaPlayerBL
     {
         private PlaylistManager _playlistManager;
         private MediaDA _mediaDA;
-        private CancellationTokenSource _tokenSource;
-        public event Action<Media> MediaChanged;
-        public event Action<string> FormatChanged;
-        private Media _currentPlayingMedia;
-        private string _currentFormat;
-
-        public int PlaySpeed { get; set; }
-        public bool IsPlaying { get; set; }
-        private int currentIndex = 0;
-        public string CurrentFormat { get => _currentFormat; set { if (_currentFormat != value) { _currentFormat = value; FormatChanged?.Invoke(value); }; } }
-        public Media CurrentPlayingMedia { get => _currentPlayingMedia; set { if (_currentPlayingMedia != value) { _currentPlayingMedia = value; MediaChanged?.Invoke(value); } } }
 
         public MediaBL()
         {
-            this._mediaDA = new MediaDA();
+            _mediaDA = new MediaDA();
             _playlistManager = new PlaylistManager();
-            _currentPlayingMedia = null;
         }
         public List<Media> LoadPlaylist(string filepath)
         {
@@ -39,65 +27,12 @@ namespace MediaPlayerBL
 
         public List<Media> LoadMedia(string[] filenames)
         {
-            currentIndex = 0;
             return _mediaDA.LoadMedia(filenames);
         }
 
         public void SavePlaylist(string filepath, List<Media> loadedMedia) => _playlistManager.SavePlaylist(_mediaDA, filepath, loadedMedia);
 
-        public void SetPlaylistTitle(string title) => _playlistManager.CurrentPlaylist.PlaylistName = title;
-
         public string GetPlaylistTitle() => _playlistManager.CurrentPlaylist.PlaylistName;
-
-        public async Task PlayMediaAsync(List<Media> loadedMedia)
-        {
-            _tokenSource = new CancellationTokenSource();
-            IsPlaying = true;
-
-            try
-            {
-                for (int i = currentIndex; i < loadedMedia.Count; i++)
-                {
-                    if (_tokenSource.IsCancellationRequested)
-                    {
-                        break;
-                    }
-
-                    _currentPlayingMedia = loadedMedia[i];
-                    _currentFormat = CurrentPlayingMedia.Format;
-
-                    if (IsVideoFormat(CurrentPlayingMedia.Format))
-                    {
-                        await Task.Delay(PlaySpeed * 1000, _tokenSource.Token);
-                    }
-                    else
-                    {
-                        await Task.Delay(PlaySpeed * 1000, _tokenSource.Token);
-                    }
-                }
-            }
-            catch (TaskCanceledException)
-            {
-                _currentPlayingMedia = null;
-                IsPlaying = false;
-
-            }
-            finally
-            {
-                IsPlaying = false;
-            }
-        }
-
-        public void PauseMedia()
-        {
-            _tokenSource.Cancel();
-            IsPlaying = false;
-        }
-
-        public void ResetIndex()
-        {
-            currentIndex = 0;
-        }
 
         public bool IsImageFormat(string format)
         {
