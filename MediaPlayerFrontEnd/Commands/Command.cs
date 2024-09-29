bc987d505b60c4754c1dc0c8b1b12f666695557e
warmlund <emeliewarmlund@gmail.com>
2024-09-29 17:03:15 +0200

    Playlist logic and styles

---
+++ MediaPlayerFrontEnd/Commands/Command.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows.Input;
+
+namespace MediaPlayerPL
+{
+    public class Command : ICommand
+    {
+        private Action methodToExecute = null;
+        private Func<bool> canMethodBeExecuted = null;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action methodToExecute, Func<bool> canMethodBeExecuted)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canMethodBeExecuted = canMethodBeExecuted;
+        }
+
+        public void Execute(object parameter)
+        {
+            methodToExecute?.Invoke();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canMethodBeExecuted == null)
+            {
+                return true;
+            }
+            else
+            {
+                return canMethodBeExecuted();
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class CommandWithParameter<T> : ICommand
+    {
+        private Action<T> methodToExecuteWithParam = null;
+        private Func<T, bool> canMethodBeExecuted = null;
+        private Action methodToExecute = null;
+
+        public event EventHandler CanExecuteChanged;
+
+        public CommandWithParameter(Action<T> methodToExecute, Func<T, bool> canMethodBeExecuted)
+        {
+            methodToExecuteWithParam = methodToExecute;
+            this.canMethodBeExecuted = canMethodBeExecuted;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter != null && parameter is T typedParameter)
+            {
+                methodToExecuteWithParam?.Invoke(typedParameter);
+            }
+            else
+            {
+                methodToExecute?.Invoke();
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter != null && parameter is T typedParameter)
+            {
+                return canMethodBeExecuted(typedParameter);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+}
--- MediaStorage/Media.cs
+++ MediaStorage/Media.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,6 @@ namespace MediaDTO
 
         [JsonProperty("Format")]
         public string Format { get; set; }
+
     }
 }
